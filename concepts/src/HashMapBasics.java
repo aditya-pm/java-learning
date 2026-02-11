773bdd3fdeee5a3301b38571a8d73096541d1600
aditya-pm <adityapmenon29@gmail.com>
2026-02-11 22:22:39 +0530

    Update HashMap basics example

--- concepts/src/HashMapBasics.java
+++ concepts/src/HashMapBasics.java
@@ -46,5 +46,18 @@ public class HashMapBasics {
         for (String key : capitalCities.keySet()) {
             System.out.println("City: " + key + ", Capital: " + capitalCities.get(key));
         }
+
+        System.out.println();
+
+        // HashMap uses objects as keys and values, to use primitive types use their wrappers instead
+        HashMap<String, Integer> people = new HashMap<>();
+
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        for (String name : people.keySet()) {
+            System.out.println("Name: " + name + ", age: " + people.get(name));
+        }
     }
 }
